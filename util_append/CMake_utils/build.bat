d314a2559eb979d4778fd2dc67bd409abf274469
unknown <zhangxinlong@of3d.com>
2022-11-28 10:22:20 +0800

    update build.bat

--- util_append/CMake_utils/build.bat
+++ util_append/CMake_utils/build.bat
@@ -1,21 +1,21 @@
-::comments
 @echo off
+set INSTALL_DIR=./install
 
-set INSTALL_DIR="./install"
 if "%1"=="" (
-    DEL .\build\CMakeCache.txt
-    cmake -S. -Bbuild  ^
-    -G"Visual Studio 17 2022" -Win64
-) ^
+    RMDIR /S /Q build
+    ::vs2017 :"Visual Studio 15 2017 Win64"    
+    ::vs2019 :"Visual Studio 16 2019" -A x64
+    ::vs2022 :"Visual Studio 17 2022" -A x64
+    cmake -Bbuild -G "Visual Studio 17 2022" -A x64 ^
+    -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR%
+)else if "%1"=="c" (
+    cmake --build build -j36 --config Release --clean-first
+)else if "%1"=="i" (
+    RMDIR /S /Q %INSTALL_DIR%
+    cmake --install build
+)else if "%1"=="r" (
+    cmake --build build --target run --config Release
+)
 
-else if "%1"=="c" (
-    cmake --build build -j8 --clean-first --config Release
-) ^
-
-else if "%1"=="i" (
-    cmake --install build --prefix %INSTALL_DIR%  
-) ^
-
-else if "%1"=="r" (
-    cmake --build build --target run
-) ^
+::build & install command
+::cmake --build build -j36 --target install --config Release
