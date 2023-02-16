d46dd5a368a3404c45753ffcbfdc9dec119c3a62
zhangxinlong <zhangxinlong@of3d.com>
2023-02-16 15:01:58 +0800

    update

--- util_append/CMake_utils/build.sh
+++ util_append/CMake_utils/build.sh
@@ -1,22 +1,26 @@
 if [ -z "$1" ]; then
 
   rm -f ./build/CMakeCache.txt
-  INSTALL_LOC=/home/simon/app/gaffer
-  cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug -GNinja \
+  INSTALL_LOC=./install
+  cmake -Bbuild -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DBOOST_ROOT=~/app/gaffer-build \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_LOC
 
 elif [ $1 = "c" ]; then
 
-  cmake --build build -j8
+  cpu_count=$(grep -c ^processor /proc/cpuinfo)
+  cmake --build build -j ${cpu_count}
 
 elif [ $1 = "i" ]; then
 
   rm -rf $INSTALL_LOC
   cmake --install build
 elif [ $1 = "r" ]; then
-
+  export LD_LIBRARY_PATH=/home/zhangxinlong/repo/test/testcpp/IE_RTTI/build:~/app/gaffer-build/lib
   cmake --build build --target run
 fi
 
 #ctest -j 12
+
 
