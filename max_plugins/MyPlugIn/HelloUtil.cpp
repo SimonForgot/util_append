b38090633cae67d3bb5ffbe3c9a47167da7d1d67
SimonForgot <zxl120125@126.com>
2025-04-15 12:34:57 +0800

    add select

--- max_plugins/MyPlugIn/HelloUtil.cpp
+++ max_plugins/MyPlugIn/HelloUtil.cpp
@@ -3,6 +3,7 @@
 #include <simpobj.h>
 #include <QtWidgets/QMessageBox>
 
+
 void HelloUtil::BeginEditParams(Interface *ip, IUtil *iu)
 {
     UI = new PluginUI();
@@ -19,10 +20,50 @@ void HelloUtil::onButtonClicked()
 {
     float value = UI->spinBox->value();
 
-    Interface17* ip = GetCOREInterface17();
+    Interface17 *ip = GetCOREInterface17();
     GenSphere *gs = (GenSphere *)ip->CreateInstance(GEOMOBJECT_CLASS_ID, Class_ID(SPHERE_CLASS_ID, 0));
     gs->SetParams(value, 240);
     INode *pNode = ip->CreateObjectNode(gs, L"Sphere");
+    ip->RedrawViews(0);
 
     QMessageBox::information(UI, "SpinBox Value", QString("Value: %1").arg(value));
+}
+
+void HelloUtil::SelectionSetChanged(Interface *ip, IUtil *iu)
+{
+    if (ip->GetSelNodeCount() > 0)
+    {
+        ReplaceReference(0, ip->GetSelNode(0));
+    }
+    else
+    {
+        ReplaceReference(0, NULL);
+    }
+}
+
+void HelloUtil::SetReference(int i, ReferenceTarget *pTarget)
+{
+    switch (i)
+    {
+    case 0: // we have only one reference in this plug-in
+    {
+        INode *tmp = dynamic_cast<INode *>(pTarget);
+        if (tmp)
+        {
+            target_node = tmp;
+            MSTR s(tmp->GetName());
+            UI->label->setText(s);
+        }
+        else{
+            target_node = nullptr;
+            UI->label->setText("No Node Selected");
+        }
+    }
+    break;
+    }
+}
+
+RefResult HelloUtil::NotifyRefChanged(const Interval &changeInt, RefTargetHandle hTarget, PartID &partID, RefMessage message, BOOL propagate)
+{
+    return REF_SUCCEED;
 }
--- max_plugins/MyPlugIn/HelloUtil.h
+++ max_plugins/MyPlugIn/HelloUtil.h
@@ -4,14 +4,17 @@
 #include <Qt/QmaxSpinBox.h>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
 
 class PluginUI : public MaxSDK::QmaxRollupContainer
 {
 public:
+	QLabel *label;
 	MaxSDK::QmaxDoubleSpinBox *spinBox;
 	QPushButton *button;
 	PluginUI() : MaxSDK::QmaxRollupContainer()
 	{
+		label = new QLabel("No Node Selected", this);
 		spinBox = new MaxSDK::QmaxDoubleSpinBox(this);
 		spinBox->setRange(0.0, 100.0);
 		spinBox->setValue(1.0);
@@ -19,21 +22,29 @@ public:
 		button = new QPushButton("OK", this);
 		QVBoxLayout *layout = new QVBoxLayout(this);
 
+		layout->addWidget(label, 0, Qt::AlignHCenter);
 		layout->addWidget(spinBox, 0, Qt::AlignHCenter);
 		layout->addWidget(button, 0, Qt::AlignHCenter);
 		setLayout(layout);
 	}
 };
 
-class HelloUtil : public UtilityObj, public QObject
+class HelloUtil : public UtilityObj, public QObject, public ReferenceMaker
 {
 private:
-	INode *target_node;
+	INode *target_node = nullptr;
 	PluginUI *UI;
-	
+	void SetReference(int i, ReferenceTarget *pTarget);
 public:
+	~HelloUtil() { DeleteAllRefsFromMe(); };
 	void BeginEditParams(Interface *ip, IUtil *iu);
 	void EndEditParams(Interface *ip, IUtil *iu);
 	void DeleteThis() {}
 	void onButtonClicked();
+
+	void SelectionSetChanged(Interface *ip, IUtil *iu) override;
+
+	int NumRefs() override { return 1; }
+	ReferenceTarget *GetReference(int i) { return target_node; }
+	RefResult NotifyRefChanged(const Interval &changeInt, RefTargetHandle hTarget, PartID &partID, RefMessage message, BOOL propagate);
 };
